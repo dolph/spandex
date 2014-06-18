a346c5faffc2f0ab34bc9601b11919af3ec6e353
Dolph Mathews <dolph.mathews@gmail.com>
2014-06-18 11:50:35 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name='spandex',
+    version='0.0.0',
+    description='Do something useful with elasticsearch.',
+    author='Dolph Mathews',
+    author_email='dolph.mathews@gmail.com',
+    url='http://github.com/dolph/spandex',
+    install_requires=[''],
+    entry_points={'console_scripts': ['spandex = spandex.cli:main']},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+    ],
+)
